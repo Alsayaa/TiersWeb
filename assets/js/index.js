2ce77d506a27eb4c75f3846109343b31fc84fec2
Alsaya <emmagddc@hotmail.com>
2025-10-30 15:48:42 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+
+
+// Bouton de retour vers le haut
+
+const btnBackToHome = document.getElementById('btn-up');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        btnBackToHome.style.opacity = '1';
+        btnBackToHome.style.visibility = 'visible';
+    } else {
+        btnBackToHome.style.opacity = '0';
+        btnBackToHome.style.visibility = 'hidden';
+    }
+});
+
+btnBackToHome.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
+
+// Toggle de la musique + stockage avec le localstorage de js pour se souvenir si l'utilisateur
+// a activé la musique ou non
+
+
+const musicToggler = document.getElementById('audio-btn');
+
+musicToggler.addEventListener('click', () => {
+
+
+});
